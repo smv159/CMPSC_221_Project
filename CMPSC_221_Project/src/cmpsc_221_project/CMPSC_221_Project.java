3f6c15526265520cdf0971b9091f6255884bd610
Seth Volpe <smv159@psu.edu>
2022-02-15 17:03:15 -0500

    Added a main method

--- CMPSC_221_Project/src/cmpsc_221_project/CMPSC_221_Project.java
+++ CMPSC_221_Project/src/cmpsc_221_project/CMPSC_221_Project.java
@@ -16,6 +16,7 @@ public class CMPSC_221_Project {
      */
     public static void main(String[] args) {
         System.out.println("Im a new branch");
+        System.out.println("Made a token");
     }
     
     
